3d66521dbf1f075238d424b0e228ddfcd43251b2
ChuguevRoman <5tiger5xman@gmail.com>
2023-03-21 11:50:09 +0200

    Added loading indicator to login_screen and registration_screen.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_flutter_test2/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../widgets/rounded_button.dart';
 import '../constants.dart';
@@ -16,12 +17,16 @@ class _LoginScreenState extends State<LoginScreen> {
   late String email;
   late String password;
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
+      body: showSpinner ? const SpinKitFadingCircle(
+        color: Colors.blueGrey,
+        size: 50.0,
+      ) : Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -69,16 +74,25 @@ class _LoginScreenState extends State<LoginScreen> {
             RoundedButton(
                 color: Colors.lightBlueAccent,
                 onTap: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
                   try{
                     final user = await _auth.signInWithEmailAndPassword(
                         email: email, password: password);
                     if (user != null) {
                       if (!mounted) return;
+                      setState(() {
+                        showSpinner = false;
+                      });
                       Navigator.pushNamed(context, ChatScreen.id);
                     }
                   }
                   catch(e) {
                     print(e);
+                    setState(() {
+                      showSpinner = false;
+                    });
                   }
                 },
                 text: 'Log In'),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../widgets/rounded_button.dart';
 import '../constants.dart';
@@ -16,75 +17,90 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   late String email;
   late String password;
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: showSpinner
+          ? SpinKitFadingCircle(
+              color: Colors.black,
+              size: 50.0,
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 48.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      //Do something with the user input.
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your Email',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    onChanged: (value) {
+                      //Do something with the user input.
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  RoundedButton(
+                      color: Colors.blueAccent,
+                      onTap: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          final newUser =
+                              await _auth.createUserWithEmailAndPassword(
+                                  email: email, password: password);
+                          if (newUser != null) {
+                            if (!mounted) return;
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          setState(() {
+                            showSpinner = false;
+                          });
+                          }
+                        } catch (e) {
+                          print(e);
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        }
+                      },
+                      text: 'Register'),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your Email',
-              ),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
-              ),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-                color: Colors.blueAccent,
-                onTap: () async {
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    if(newUser != null) {
-                      if(!mounted) return;
-                      Navigator.pushNamed(context, ChatScreen.id);
-                    }
-                  }
-                  catch(e) {
-                    print(e);
-                  }
-                },
-                text: 'Register'),
-          ],
-        ),
-      ),
     );
   }
 }
